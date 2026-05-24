bf615e56e3b432b6b8fc78758d9719f6faeb17f5
Achim Setti John Benny <123yojohn@gmail.com>
2026-05-25 00:08:33 +0530

    Fixed frontend backend connection

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://payflow-backend-7ylu.onrender.com",
 });
 
 export default API;
